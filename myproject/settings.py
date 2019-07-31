66044009e6f88fb304d17fbfb2e11e21b4bbd0d5
musung9678 <pmseng@naver.com>
2019-07-31 18:10:10 +0900

    dfdfs

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','1ypkw4ks1m%ks)c=qay8m07juzs&+zv()h2r*!^xm)h7#+t@w8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get('DJANGO_DEBUG',True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']   
 
 
 # Application definition
